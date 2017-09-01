bb3888c567fa13a25cc5b41fbead81efc281c3c6
Laurent Jérôme (DI) <jerome.laurent@etat.ge.ch>
2017-09-01 09:57:53 +0000

    git-svn-id: http://melusineicprod01.ceti.etat-ge.ch:21080/cti.composant/ct-gina-ldap-client/trunk@55734 222efd1c-12ae-ee84-b859-f398d68c9be8

--- src/test/java/gina/api/GinaApiLdapApplicationTest.java
+++ src/test/java/gina/api/GinaApiLdapApplicationTest.java
@@ -8,82 +8,89 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class GinaApiLdapApplicationTest {
 
+    // Logger
     private static final Logger LOG = Logger.getLogger(GinaApiLdapApplicationTest.class);
 
     @Test
     public void getUserRolesTest() {
-	LOG.info("getUserRolesTest");
+	LOG.info(GinaApiLdapContantsTest.START_METHOD);
 
 	try {
-	    List<String> ret = GinaApiLdapBaseFactory.getInstanceApplication().getUserRoles("DRIVONOL");
-	    assertSame(13, ret.size());
+	    List<String> roles = GinaApiLdapBaseFactory.getInstanceApplication().getUserRoles(GinaApiLdapContantsTest.DRIVONOL_USERNAME);
+	    Assert.assertNotNull(roles);
+	    Assert.assertTrue(roles.size() > 0);
+	    LOG.info("roles.size()=" + roles.size());
+	    LOG.info("roles=" + roles);
+	    Assert.assertTrue(roles.contains("UTILISATEUR"));
+	    Assert.assertTrue(roles.contains("CONTEXTE-APA"));
 	} catch (GinaException e) {
+	    LOG.error(e);
 	    assertTrue(false);
 	} catch (RemoteException e) {
+	    LOG.error(e);
 	    assertTrue(false);
 	}
     }
 
     @Test
     public void getIsValidUserTest() {
-	LOG.info("getIsValidUserTest");
+	LOG.info(GinaApiLdapContantsTest.START_METHOD);
 
 	try {
-
-	    boolean ret = GinaApiLdapBaseFactory.getInstanceApplication().isValidUser("DRIVONOL");
-	    LOG.info("user valid " + ret);
-
-	    if (!ret) {
-		assertTrue(false);
-	    } else {
-		assertTrue(true);
-	    }
-
+	    // Utilisateur valide
+	    boolean result = GinaApiLdapBaseFactory.getInstanceApplication().isValidUser(GinaApiLdapContantsTest.DRIVONOL_USERNAME);
+	    LOG.info("result=" + result);
+	    assertTrue(result);
+	    
+	    // Utilisateur non valide
+	    result = GinaApiLdapBaseFactory.getInstanceApplication().isValidUser(GinaApiLdapContantsTest.PINAUDJ_USERNAME);
+	    LOG.info("result=" + result);
+	    assertTrue(!result);
 	} catch (GinaException e) {
+	    LOG.error(e);
 	    assertTrue(false);
 	} catch (RemoteException e) {
+	    LOG.error(e);
 	    assertTrue(false);
 	}
-
     }
 
     @Test
     public void getUserAttrsTest() {
-	LOG.info("getUserAttrsTest");
+	LOG.info(GinaApiLdapContantsTest.START_METHOD);
 
 	try {
-	    String[] paramArrayOfString = { "uid" };
-	    Map<String, String> ret = GinaApiLdapBaseFactory.getInstanceApplication().getUserAttrs("DRIVONOL",
-		    paramArrayOfString);
-	    for (Map.Entry<String, String> e : ret.entrySet()) {
-		LOG.info(e.getKey() + " : " + e.getValue());
-		if (e.getKey().equalsIgnoreCase("uid")) {
-		    assertTrue(e.getValue().equalsIgnoreCase("DRIVONOL"));
+	    Map<String, String> user = GinaApiLdapBaseFactory.getInstanceApplication().getUserAttrs(GinaApiLdapContantsTest.DRIVONOL_USERNAME,
+		    GinaApiLdapContantsTest.TEST_ATTRS);
+	    for (Map.Entry<String, String> e : user.entrySet()) {
+		LOG.info(e.getKey() + "=" + e.getValue());
+		if ("uid".equalsIgnoreCase(e.getKey())) {
+		    assertTrue(e.getValue().equalsIgnoreCase(GinaApiLdapContantsTest.DRIVONOL_USERNAME));
+		    break;
 		}
-
 	    }
-	    LOG.info("user valid " + ret);
-
+	    LOG.info("user=" + user);
 	} catch (GinaException e) {
+	    LOG.error(e);
 	    assertTrue(false);
 	} catch (RemoteException e) {
+	    LOG.error(e);
 	    assertTrue(false);
 	}
     }
 
     @Test
     public void hasRoleTest() {
-	LOG.info("hasRoleTest");
+	LOG.info(GinaApiLdapContantsTest.START_METHOD);
 
 	try {
-	    String[] paramArrayOfString = { "uid" };
 	    boolean ret = GinaApiLdapBaseFactory.getInstanceApplication().hasRole("ADMIN");
 	    assertSame(false, ret);
-
 	} catch (GinaException e) {
 	    assertTrue(false);
 	} catch (RemoteException e) {
@@ -93,12 +100,11 @@ public class GinaApiLdapApplicationTest {
 
     @Test
     public void hasRoleUserTest() {
-	LOG.info("hasRoleUserTest");
+	LOG.info(GinaApiLdapContantsTest.START_METHOD);
 
 	try {
-	    boolean ret = GinaApiLdapBaseFactory.getInstanceApplication().hasUserRole("DRIVONOL", "ADMIN");
+	    boolean ret = GinaApiLdapBaseFactory.getInstanceApplication().hasUserRole(GinaApiLdapContantsTest.DRIVONOL_USERNAME, "ADMIN");
 	    assertSame(true, ret);
-
 	} catch (GinaException e) {
 	    assertTrue(false);
 	} catch (RemoteException e) {
@@ -108,12 +114,12 @@ public class GinaApiLdapApplicationTest {
 
     @Test
     public void getRolesTest() {
-	LOG.info("getRolesTest");
+	LOG.info(GinaApiLdapContantsTest.START_METHOD);
 
 	try {
 	    List<String> ret = GinaApiLdapBaseFactory.getInstanceApplication().getRoles();
 	    assertSame(0, ret.size());
-
+	    // La recherche de roles se fait avec l'utilisateur specifié dans le fichier properties (TCNXXX) donc ça doit retourner 0 
 	} catch (GinaException e) {
 	    assertTrue(false);
 	} catch (RemoteException e) {
@@ -123,12 +129,15 @@ public class GinaApiLdapApplicationTest {
 
     @Test
     public void getAppRolesTest() {
-	LOG.info("getAppRolesTest");
+	LOG.info(GinaApiLdapContantsTest.START_METHOD);
 
 	try {
-	    List<String> ret = GinaApiLdapBaseFactory.getInstanceApplication().getAppRoles("CAMAC-GENEVE");
-	    assertSame(21, ret.size());
-
+	    List<String> roles = GinaApiLdapBaseFactory.getInstanceApplication().getAppRoles("CAMAC-GENEVE");
+	    Assert.assertNotNull(roles);
+	    Assert.assertTrue(roles.size() > 0);
+	    LOG.info("roles.size()=" + roles.size());
+	    LOG.info("roles=" + roles);
+	    Assert.assertTrue(roles.contains("ADMIN"));
 	} catch (GinaException e) {
 	    assertTrue(false);
 	} catch (RemoteException e) {
---
+++ src/test/java/gina/api/GinaApiLdapContantsTest.java
@@ -0,0 +1,20 @@
+package gina.api;
+
+public class GinaApiLdapContantsTest {
+
+    // Utilisateur PINAUDJ
+    protected static final String PINAUDJ_USERNAME = "PINAUDJ";
+
+    // Utilisateur DTDCOURS01
+    protected static final String DTDCOURS01_USERNAME = "DTDCOURS01";
+
+    // Utilisateur DRIVONOL
+    protected static final String DRIVONOL_USERNAME = "DRIVONOL";
+
+    // Propriétés de l'utilisateur
+    protected static String[] TEST_ATTRS = { "initials", "givenName", "sn", "username", "uid" };
+
+    // String indiquant le début d'un test
+    protected static final String START_METHOD = "START";
+
+}
