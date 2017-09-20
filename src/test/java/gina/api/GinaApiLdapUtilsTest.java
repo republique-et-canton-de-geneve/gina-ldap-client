e337cf4f3c8f4a27fbd957e4bc57db804cb9f926
Laurent Jérôme (DI) <jerome.laurent@etat.ge.ch>
2017-09-20 13:53:34 +0000

    git-svn-id: http://melusineicprod01.ceti.etat-ge.ch:21080/cti.composant/ct-gina-ldap-client/trunk@56058 222efd1c-12ae-ee84-b859-f398d68c9be8

---
+++ src/test/java/gina/api/GinaApiLdapUtilsTest.java
@@ -0,0 +1,19 @@
+package gina.api;
+
+import java.util.List;
+
+public class GinaApiLdapUtilsTest {
+
+    protected static boolean checkRolesAreCleaned(final List<String> roles) {
+	boolean result = true;    
+	for(String role : roles) {
+	    if(role.contains("cn=")) {
+		result = false;
+		break;
+	    }
+	}
+	return result;
+    }
+    
+
+}
